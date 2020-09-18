dd0bf159332266b87c7d3ec32ba41dd4fa607fa9
andyfu <815965325@qq.com>
2020-09-19 00:37:37 +0800

    first commit Plugin Project

---
+++ Plugin/app/src/main/java/com/fxj/Plugin/PluginMainActivity.kt
@@ -0,0 +1,11 @@
+package com.fxj.Plugin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PluginMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
