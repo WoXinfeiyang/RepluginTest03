12c402ca2436bb12f50eeb029c5a2c8576a4a43a
andyfu <815965325@qq.com>
2020-09-19 00:05:31 +0800

    1、安装插件。2、为了监听插件是否按照成功，在调用RePlugin.App.attachBaseContext方法时传入自定义的RePluginEventCallbacks

--- RepluginHost/app/src/main/java/com/fxj/repluginhost/BaseApplication.kt
+++ RepluginHost/app/src/main/java/com/fxj/repluginhost/BaseApplication.kt
@@ -2,14 +2,54 @@ package com.fxj.repluginhost
 
 import android.app.Application
 import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
+import android.util.Log
 import com.qihoo360.replugin.RePlugin
+import com.qihoo360.replugin.RePluginConfig
+import com.qihoo360.replugin.RePluginEventCallbacks
+import com.qihoo360.replugin.model.PluginInfo
 
 class BaseApplication: Application() {
 
+    companion object{
+        val TAG=BaseApplication::class.java.simpleName
+    }
+
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
-        RePlugin.App.attachBaseContext(this)
+        var rePluginConfig=RePluginConfig()
+        rePluginConfig.setEventCallbacks(object: RePluginEventCallbacks(base) {
+            val TAG:String="RPEventCallbacks_fxj"
+
+            override fun onInstallPluginFailed(path: String?, code: InstallResult?) {
+                Log.d(TAG,"##onInstallPluginFailed##path=${path},InstallResult=${code}")
+            }
+
+            override fun onInstallPluginSucceed(info: PluginInfo?) {
+                Log.d(TAG,"##onInstallPluginSucceed##PluginInfo=${info}")
+            }
+
+            override fun onPrepareAllocPitActivity(intent: Intent?) {
+                Log.d(TAG,"##onPrepareAllocPitActivity##,当插件Activity准备分配坑位时执行该方法,Intent=${intent}")
+            }
+
+            override fun onPrepareStartPitActivity(
+                context: Context?,
+                intent: Intent?,
+                pittedIntent: Intent?
+            ) {
+                Log.d(TAG,"##onPrepareStartPitActivity##intent=${intent},pittedIntent=${pittedIntent}")
+            }
+            override fun onStartActivityCompleted(
+                plugin: String?,
+                activity: String?,
+                result: Boolean
+            ) {
+                Log.d(TAG,"##onStartActivityCompleted##plugin=${plugin},activity=${activity},result=${result}")
+            }
+        })
+        RePlugin.App.attachBaseContext(this,rePluginConfig)
     }
 
     override fun onCreate() {
