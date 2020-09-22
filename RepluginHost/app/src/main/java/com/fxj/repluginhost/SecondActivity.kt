55a70178339125bccd6e25e9f4f4afd1aa35ab58
andyfu <815965325@qq.com>
2020-09-22 17:25:55 +0800

    宿主加载显示插件里面的图片

--- RepluginHost/app/src/main/java/com/fxj/repluginhost/SecondActivity.kt
+++ RepluginHost/app/src/main/java/com/fxj/repluginhost/SecondActivity.kt
@@ -4,9 +4,12 @@ import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import com.qihoo360.replugin.RePlugin
 
-class SecondActivity : Activity() {
+class SecondActivity : Activity(), View.OnClickListener {
     companion object{
         val TAG:String=SecondActivity::class.java.simpleName+"_fxj"
         val DIR_PATH="RepluginHost"
@@ -19,14 +22,35 @@ class SecondActivity : Activity() {
     var pluginContext: Context?=null
     var pluginClassLoader:ClassLoader?=null
 
+    var image:ImageView?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        findViewById<Button>(R.id.seconde_act_btn01).setOnClickListener(this)
+
+        image=findViewById(R.id.seconde_act_iv)
 
         pluginContext=RePlugin.fetchContext(PLUGIN_NAME)
         pluginClassLoader=RePlugin.fetchClassLoader(PLUGIN_NAME)
 
         Log.d(TAG, "##onCreate##pluginContext canonicalName=${pluginContext?.javaClass?.canonicalName},pluginClassLoader canonicalName=${pluginClassLoader?.javaClass?.canonicalName}")
 
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.seconde_act_btn01->{
+                if(pluginContext!=null){
+                    var drawable= pluginContext?.getResources()?.getDrawable(
+                        pluginContext?.getResources()!!
+                            .getIdentifier("mickey_mouse_in_plugin", "drawable","com.fxj.Plugin"))
+                    if(drawable!=null&&image!=null){
+                        image?.setImageDrawable(drawable)
+                    }
+                }
+            }
+        }
     }
 }
