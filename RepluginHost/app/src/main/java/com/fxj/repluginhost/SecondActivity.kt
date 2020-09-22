a65076ce48596fc7003d33bc3b22e32597dee35d
andyfu <815965325@qq.com>
2020-09-22 14:04:21 +0800

    获取插件的Context和ClassLoader

--- RepluginHost/app/src/main/java/com/fxj/repluginhost/SecondActivity.kt
+++ RepluginHost/app/src/main/java/com/fxj/repluginhost/SecondActivity.kt
@@ -1,11 +1,32 @@
 package com.fxj.repluginhost
 
 import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import com.qihoo360.replugin.RePlugin
 
 class SecondActivity : Activity() {
+    companion object{
+        val TAG:String=SecondActivity::class.java.simpleName+"_fxj"
+        val DIR_PATH="RepluginHost"
+        val PLUGIN_FILE_NAME="com.fxj.Plugin.apk"
+
+        /**插件名称,"纯Apk插件"的插件名为插件的PackageName*/
+        val PLUGIN_NAME="com.fxj.Plugin"
+    }
+
+    var pluginContext: Context?=null
+    var pluginClassLoader:ClassLoader?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        pluginContext=RePlugin.fetchContext(PLUGIN_NAME)
+        pluginClassLoader=RePlugin.fetchClassLoader(PLUGIN_NAME)
+
+        Log.d(TAG, "##onCreate##pluginContext canonicalName=${pluginContext?.javaClass?.canonicalName},pluginClassLoader canonicalName=${pluginClassLoader?.javaClass?.canonicalName}")
+
     }
 }
